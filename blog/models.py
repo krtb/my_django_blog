48cb720a69a527832182a2a3ed88f9bfd7a1c61c
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-17 14:27:23 -0500

    add max char length for title

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,4 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
