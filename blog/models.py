3840578bc3e2ed2201eca9b7a4f1af0056f45fc8
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-17 14:31:59 -0500

    add default date/time

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,4 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # link to another model
     title = models.CharField(max_length=200) #define text with limited amount of characters
     text = models.TextField() #long text with out limit
+    create_date = models.DateTimeField(default=timezone.now) 
