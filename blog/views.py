05e6fba69b6b7e57a2ade92cbd385eb140935a85
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-24 13:32:37 -0500

    use RequestContext

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.template import RequestContext
 from .models import Post
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(RequestContext(request), 'blog/post_list.html', {'posts': posts})
